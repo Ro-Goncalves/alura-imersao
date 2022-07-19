1a8a458bfc10d0eb04ddf7d9c1da87b6e7ca8034
Rodrigo Gonçalves <ro.go.calves@gmail.com>
2022-07-19 13:23:55 -0300

    Aula 02 Desafio 09: Finalizado

--- src/gerador/figuras/GeradorDeFigurinhas.java
+++ src/gerador/figuras/GeradorDeFigurinhas.java
@@ -14,11 +14,20 @@ import javax.imageio.ImageIO;
 
 public class GeradorDeFigurinhas {
 	
-	private String absPath = new File("").getAbsolutePath();
+	private String absPath = new File("").getAbsolutePath();	
 	
-	public void criar(InputStream input, String nomeArquivo) {
+	public String textoImagem(String voto) {
+		
+		return voto.equals("8.5") ? "The Third One" : 
+               voto.equals("8.6") ? "The Second One" : 
+	           voto.equals("8.7") ? "The First One" :
+		                            "The Last One";
+	}
+	
+	public void criar(InputStream input, String nomeArquivo, String voto) {
 		String caminhoImagem = absPath + File.separator + "img" + File.separator;
 		String nomeImagem = nomeArquivo + ".png";
+		String textoImagem = textoImagem(voto);
 		
 		try {			
 			BufferedImage imagemMemoria = ImageIO.read(input);
@@ -32,9 +41,9 @@ public class GeradorDeFigurinhas {
 			Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
 			graphics.drawImage(imagemMemoria, 0, 0, null);
 			
-			graphics.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 64));
+			graphics.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
 			graphics.setColor(Color.DARK_GRAY);
-			graphics.drawString("The Big One", 100, novaAltura - 100);
+			graphics.drawString(textoImagem, 0, novaAltura - 100);
 			
 			ImageIO.write(novaImagem, "png", new File(caminhoImagem + nomeImagem));
 		} catch (IOException e) {			
@@ -47,7 +56,7 @@ public class GeradorDeFigurinhas {
 		try {
 			String nomeImagem = "O Senhor dos Anéis - O Retorno do Rei";
 			InputStream url = new URL("https://image.tmdb.org/t/p/w500/izPNMzffsgZUvlbiYlPxjFr3TAa.jpg").openStream();
-			gerar.criar(url, nomeImagem);
+			gerar.criar(url, nomeImagem, "8.6");
 			
 		} catch (MalformedURLException e) {			
 			e.printStackTrace();
--- src/sticker/app/AppStickerFromFile.java
+++ src/sticker/app/AppStickerFromFile.java
@@ -1,24 +1,37 @@
 package sticker.app;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
+import gerador.figuras.GeradorDeFigurinhas;
 import sticker.common.util.UtilJson;
 
 public class AppStickerFromFile {	
 	
-	public static void main(String[] args) throws ParseException {	
+	public static void main(String[] args) throws ParseException, MalformedURLException, IOException {	
 		UtilJson utilJson = new UtilJson();
 		JSONArray topRated = utilJson.getArquivoJson("themoviedb-topRated");
+		GeradorDeFigurinhas figuras = new GeradorDeFigurinhas();
 		
 		for(Object filme: topRated) {
 			JSONObject jsFilme = (JSONObject) filme;
+			String titulo = (String) jsFilme.get("title");
+			String url = "https://image.tmdb.org/t/p/w500" + jsFilme.get("poster_path");
+			String voto = jsFilme.get("vote_average").toString();
+			
+			InputStream inputUrl = new URL(url).openStream();
+			figuras.criar(inputUrl, titulo.replace(":", " -"), voto);
 			
 			System.out.println("Título Original: " + jsFilme.get("original_title"));
-			System.out.println("Título: " + jsFilme.get("title"));
-			System.out.println("Imagem: https://image.tmdb.org/t/p/w500" + jsFilme.get("poster_path"));
-			System.out.println("Avaliação: " + jsFilme.get("vote_average"));
+			System.out.println("Título: " + titulo.replace(":", " -"));
+			System.out.println("Imagem: " + url);
+			System.out.println("Avaliação: " + voto);
 			System.out.println();
 		}
 	}
